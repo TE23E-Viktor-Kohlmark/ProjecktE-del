f43fbff02ee5c3bac4378c6b07e7bb867c71ef1d
Viktor Köhlmark <viktor.kohlmark@elev.ga.ntig.se>
2026-05-11 13:05:58 +0200

    Magazine tillagd

---
+++ edel/src/main/java/server/Magazine.java
@@ -0,0 +1,16 @@
+package server;
+
+public class Magazine extends LibraryItem {
+
+    private int issueNumber; 
+    private String category; 
+    private int publishYear; 
+
+    public Magazine( String id, String title, int issueNumber, String category, int publishYear, boolean isAvailable) {
+        super(id, title, isAvailable);
+        this.issueNumber = issueNumber;
+        this.category = category; 
+        this.publishYear = publishYear;
+    }
+//  TODO: to string 
+}
