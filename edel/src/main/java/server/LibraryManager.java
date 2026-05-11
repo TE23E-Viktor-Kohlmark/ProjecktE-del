59845ff37a99a8d3439f77738bb7c5e0da788ed6
Viktor Köhlmark <viktor.kohlmark@elev.ga.ntig.se>
2026-05-11 13:11:41 +0200

    LibraryManager existerar

--- edel/src/main/java/server/LibraryManager.java
+++ edel/src/main/java/server/LibraryManager.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
 package server;
 
 public class LibraryManager {
 
+    private List<LibraryItem> items; 
+
+    public LibraryManager() {
+        this.items = new ArrayList<>(); 
+    }
+
+public void addItem(LibraryItem item) {
+        items.add(item);
+    }
 
     
 }
